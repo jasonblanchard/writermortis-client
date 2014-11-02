a4672204f76639303b4976d7ecc5be00e5d760fd
Jason Blanchard <jas.blanchard@gmail.com>
2014-11-02 09:41:18 -0500

    Can create new pieces

--- app/controllers/story.js
+++ app/controllers/story.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  newPiece: function() {
+    return this.store.createRecord('piece');
+  }.property('model'),
+
+  actions: {
+    createPiece: function() {
+      var controller = this;
+      var piece = this.get('newPiece');
+
+      // TODO Extract this currentUser stuff into something else
+      var session = controller.get('session');
+      var userId = session.get('content').user_id;
+      var story = controller.get('model');
+      controller.store.find('user', userId).then(function(user) {
+        piece.set('user', user);
+        piece.set('story', story);
+
+        piece.save().then(function(piece) {
+          controller.get('model.pieces').addObject(piece);
+        });
+      });
+    }
+  }
 });
