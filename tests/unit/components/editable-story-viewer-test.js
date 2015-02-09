b145e318c30034d2b8b59267c6ca31bd96d8615b
Jason Blanchard <jas.blanchard@gmail.com>
2015-02-08 19:10:04 -0500

    Animation working

--- tests/unit/components/editable-story-viewer-test.js
+++ tests/unit/components/editable-story-viewer-test.js
@@ -5,7 +5,7 @@ import {
 
 moduleForComponent('editable-story-viewer', 'EditableStoryViewerComponent', {
   // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+  needs: ['helper:formatted-text']
 });
 
 test('it renders', function() {
