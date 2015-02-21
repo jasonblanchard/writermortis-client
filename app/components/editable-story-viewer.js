73065c7d15c6ba26168f7ba6401b5b23e5035750
Jason Blanchard <jas.blanchard@gmail.com>
2015-02-21 18:22:12 -0500

    Validating piece has text

--- app/components/editable-story-viewer.js
+++ app/components/editable-story-viewer.js
@@ -2,6 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  isEmpty: function() {
+    if (this.get('newPiece').get('text')) {
+      return this.get('newPiece').get('text').length <= 0;
+    } else {
+      return true;
+    }
+  }.property('newPiece.text'),
+
   actions: {
 
     createPiece: function() {
