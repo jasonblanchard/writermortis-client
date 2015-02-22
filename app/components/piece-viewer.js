79f9b981395b28411ce50328d7d170a23ed6610d
Jason Blanchard <jas.blanchard@gmail.com>
2015-02-22 12:26:28 -0500

    Changed compoenent tag name

--- app/components/piece-viewer.js
+++ app/components/piece-viewer.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'span',
   mouseEnter: function() {
     var authorId = this.$('span').data('author');
     this.sendAction('setActiveAuthor', authorId);
