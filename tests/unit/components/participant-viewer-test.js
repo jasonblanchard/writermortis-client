e98fc7ac323cea834e0dcc236d0bddad48cf3a8e
Jason Blanchard <jas.blanchard@gmail.com>
2015-02-22 12:09:21 -0500

    Added test

--- tests/unit/components/participant-viewer-test.js
+++ tests/unit/components/participant-viewer-test.js
@@ -3,19 +3,35 @@ import {
   test
 } from 'ember-qunit';
 
+import Ember from 'ember';
+
 moduleForComponent('participant-viewer', 'ParticipantViewerComponent', {
   // specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
 });
 
+var User = Ember.Object.extend();
+var participant = User.create({id: '1'});
+
+
 test('it renders', function() {
   expect(2);
 
   // creates the component instance
-  var component = this.subject();
+  var component = this.subject({participant: participant, activeAuthorId: 1});
   equal(component._state, 'preRender');
 
   // appends the component to the page
   this.append();
   equal(component._state, 'inDOM');
+});
+
+test('#isActive when author is active', function() {
+  var component = this.subject({participant: participant, activeAuthorId: 1});
+  equal(component.get('isActive'), "active");
+});
+
+test('#isActive when author is not active', function() {
+  var component = this.subject({participant: participant, activeAuthorId: 2});
+  equal(component.get('isActive'), '');
 });
