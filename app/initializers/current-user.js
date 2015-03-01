e523d5904fe8f3c9a0074e74ce0f578b3fa0921b
Jason Blanchard <jas.blanchard@gmail.com>
2015-03-01 11:27:03 -0500

    Fixed initializer test

--- app/initializers/current-user.js
+++ app/initializers/current-user.js
@@ -1,17 +1,19 @@
 import Ember from "ember";
 import Session from "simple-auth/session";
 
+export function initialize(container) {
+  Session.reopen({
+    currentUser: function() {
+      var userId = this.get('user_id');
+      if (!Ember.isEmpty(userId)) {
+        return container.lookup('store:main').find('user', userId);
+      }
+    }.property('user_id')
+  });
+}
+
 export default {
   name: 'current-user',
   before: 'simple-auth',
-  initialize: function(container) {
-    Session.reopen({
-      currentUser: function() {
-        var userId = this.get('user_id');
-        if (!Ember.isEmpty(userId)) {
-          return container.lookup('store:main').find('user', userId);
-        }
-      }.property('user_id')
-    });
-  }
+  initialize: initialize
 };
