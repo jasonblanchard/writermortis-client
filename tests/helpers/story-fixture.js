1ff1dd637fb90a66c815ff5e68a6e98083a144da
Jason Blanchard <jas.blanchard@gmail.com>
2015-03-02 08:25:26 -0500

    Added tests for creating a story

---
+++ tests/helpers/story-fixture.js
@@ -0,0 +1,28 @@
+export default {
+    "pieces": [
+        {
+            "id": 1,
+            "text": "Once upon a time",
+            "created_at": "2014-10-22T00:33:35.283Z",
+            "updated_at": "2014-10-22T00:33:35.283Z",
+            "user_id": 1
+        }
+    ],
+    "users": [
+        {
+            "id": 1,
+            "email": "jason@example.com"
+        }
+    ],
+    "story": {
+      "id": 2,
+      "title": "Great New Story",
+      "total_pieces": 6,
+      "max_sentences": 3,
+      "piece_ids": [
+        1,
+        2
+      ],
+      "user_id": 1
+    }
+};
---
+++ tests/integration/create-story-test.js
@@ -0,0 +1,67 @@
+import Ember from 'ember';
+import startApp from 'writermortis/tests/helpers/start-app';
+import Pretender from 'pretender';
+import storiesFixtures from 'writermortis/tests/helpers/stories-fixtures';
+import userFixtures from 'writermortis/tests/helpers/user-fixtures';
+import storyFixture from 'writermortis/tests/helpers/story-fixture';
+import pieceFixture from 'writermortis/tests/helpers/piece-fixture';
+import mockResponse from 'writermortis/tests/helpers/mock-response';
+
+var App;
+var server;
+
+module('Integration - create a story', {
+  setup: function() {
+    App = startApp();
+
+    server = new Pretender(function() {
+      this.get("/api/v1/stories", function(request) {
+        return mockResponse.ok(storiesFixtures);
+      });
+
+      this.get("/api/v1/users/1", function(request) {
+        return mockResponse.ok(userFixtures.jason);
+      });
+
+      this.get("/api/v1/users/2", function(request) {
+        return mockResponse.ok(userFixtures.lucille);
+      });
+
+      this.post("/api/v1/stories", function(request) {
+        return mockResponse.ok(storyFixture);
+      });
+
+      this.post("/api/v1/pieces", function(request) {
+        return mockResponse.ok(pieceFixture);
+      });
+    });
+  },
+  teardown: function() {
+    Ember.run(App, 'destroy');
+    server.shutdown();
+  }
+});
+
+test("Should allow a logged in user to create a story", function() {
+  authenticateSession();
+  currentSession().set('currentUser', Ember.Object.create({id: "1"}));
+
+  visit('/stories/new');
+  fillIn("[name='title']", "Great New Story");
+  fillIn("[name='total-pieces']", 3);
+  fillIn("[name='max-sentences']", 2);
+  fillIn("[name='first-piece']", "And then it all got crazy");
+  click("[type='submit']");
+
+  andThen(function() {
+    equal(/Great New Story/.test(find('.story h2').text()), true);
+  });
+});
+
+test("Should redirect to login for anon users", function() {
+  invalidateSession();
+
+  visit('/stories/new').then(function() {
+    equal(find('label[for="identification"]').text(), 'Login');
+  });
+});
