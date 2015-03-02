6f025f97f6d7a34df39b1297995ebc62908aec13
Jason Blanchard <jas.blanchard@gmail.com>
2015-03-02 07:47:25 -0500

    Added tests for delete story button

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,11 +5,11 @@ export default Ember.Route.extend(ApplicationRouteMixin, {
 
   actions: {
     sessionAuthenticationSucceeded: function() {
-      this.transitionToRoute('stories');
+      this.transitionTo('stories');
     },
 
     sessionInvalidationSucceeded: function() {
-      this.transitionToRoute('stories');
+      this.transitionTo('stories');
     }
   }
 });
--- tests/integration/editable-story-test.js
+++ tests/integration/editable-story-test.js
@@ -39,7 +39,7 @@ module('Integration - editable story', {
 test("Should show the story to an anon user", function() {
   invalidateSession();
   visit('/stories/2').then(function() {
-    equal(find('.story h2').text(), "Second Story");
+    equal($.trim(find('.story h2').text()), "Second Story");
     equal($.trim(find('.last-piece').text()), 'there was a little cat named hamburger');
     equal($.trim(find(".next-action").text()), 'Sign in the add to this story!');
     equal($.trim(find('.piece-stats h2').text()), '2 / 6 Pieces');
@@ -48,13 +48,13 @@ test("Should show the story to an anon user", function() {
 
 test("Should show the story to user who posted last piece", function() {
   authenticateSession();
-  //currentSession().set('content', {user_id: .});
-  currentSession().set('currentUser', Ember.Object.create({id: 1}));
+  currentSession().set('currentUser', Ember.Object.create({id: "1"}));
 
   visit('/stories/2').then(function() {
-    equal(find('.story h2').text(), "Second Story");
+    equal(/Second Story/.test($.trim(find('.story h2').text())), true);
     equal($.trim(find('.last-piece').text()), 'there was a little cat named hamburger');
     equal($.trim(find(".next-action").text()), 'You added the last piece!');
+    equal(find('.delete-story').text(), "Delete");
   });
 });
 
@@ -63,7 +63,7 @@ test("Should show the story to a user who can post a piece", function() {
   currentSession().set('content', {user_id: 2});
 
   visit('/stories/1').then(function() {
-    equal(find('.story h2').text(), "My first story");
+    equal($.trim(find('.story h2').text()), "My first story");
     equal($.trim(find('.last-piece').text()), 'This is the start of the second story');
     equal($.trim(find(".next-action button").text()), 'Create Piece');
   });
