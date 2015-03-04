1f1c7e6307c42def0709a663adae67ca42096a07
Jason Blanchard <jas.blanchard@gmail.com>
2015-03-04 07:21:44 -0500

    trying to get validations working

--- app/controllers/stories/new.js
+++ app/controllers/stories/new.js
@@ -1,18 +1,25 @@
 import Ember from 'ember';
+import EmberValidations from 'ember-validations';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(EmberValidations.Mixin, {
   needs: 'application',
   currentUser: Ember.computed.alias('application.currentUser'),
-  newStory: function() {
-    return this.store.createRecord('story');
-  }.property(),
+  newStory: Ember.computed.alias('model'),
 
   firstPiece: function() {
     return this.store.createRecord('piece');
   }.property(),
 
+  validations: {
+    'title': {
+      'presence': true,
+      'length': { maximum: 50 }
+    }
+  },
+
   actions: {
     createStory: function() {
+      debugger;
       var controller = this;
       var story = this.get('newStory');
       var piece = this.get('firstPiece');
