efbf8d4cd0e8bc6e78b57f416cdb8e19bca7be38
Jason Blanchard <jas.blanchard@gmail.com>
2015-03-09 21:27:21 -0400

    Fixed broken code

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,13 +3,6 @@ import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
 
-  beforeModel: function() {
-    if (this.get('session.isAuthenticated')) {
-      this.transitionTo('stories.index');
-    }
-    this._super();
-  },
-
   actions: {
     sessionAuthenticationSucceeded: function() {
       this.transitionTo('stories');
