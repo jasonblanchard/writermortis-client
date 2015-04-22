63f62433f1d28bdb27e75f4528bbd5c6361a1691
Jason Blanchard <jas.blanchard@gmail.com>
2015-04-22 08:49:12 -0400

    Refactored some css

--- app/components/participant-viewer.js
+++ app/components/participant-viewer.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNameBindings: ['isActive'],
-  classNames: ['participant-viewer', 'participant'],
+  classNames: ['participant-viewer', 'participant', 'media'],
   isActive: function() {
     if (this.get('participant').get('id') === String(this.get('activeAuthorId'))) {
       return 'active';
